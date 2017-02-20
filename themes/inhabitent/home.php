a283c7aa9585f63c4eff0f365fa6d864c47138d4
Warren Chan <warrenchan@Warrens-MacBook-Pro.local>
2017-02-19 22:21:38 -0800

    jounal page

--- themes/inhabitent/home.php
+++ themes/inhabitent/home.php
@@ -10,41 +10,42 @@
  */
 
 get_header(); ?>
+	<div class="journal">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+			<?php if ( have_posts() ) : ?>
 
-		<?php if ( have_posts() ) : ?>
+				<header class="page-header">
+					<!--<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_description( '<div class="taxonomy-description">', '</div>' );
+					?>-->
+				</header><!-- .page-header -->
 
-			<header class="page-header">
-				<!--<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>-->
-			</header><!-- .page-header -->
+				<div class="post-container">
+					<?php /* Start the Loop */ ?>
+					<?php while ( have_posts() ) : the_post(); ?>
 
-            <div class="post-container">
-                <?php /* Start the Loop */ ?>
-			    <?php while ( have_posts() ) : the_post(); ?>
+						<?php
+							get_template_part( 'template-parts/content' );
+						?>
 
-				    <?php
-					    get_template_part( 'template-parts/content' );
-				    ?>
+					<?php endwhile; ?>
 
-			    <?php endwhile; ?>
+					<?php the_posts_navigation(); ?>
+				
 
-			    <?php the_posts_navigation(); ?>
-            
+					<?php else : ?>
 
-		        <?php else : ?>
+						<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
-			        <?php get_template_part( 'template-parts/content', 'none' ); ?>
+					<?php endif; ?>
+				</div>
+				<?php get_sidebar(); ?>
 
-		        <?php endif; ?>
-            </div>
-            <?php get_sidebar(); ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div>
 
 <?php get_footer(); ?>
