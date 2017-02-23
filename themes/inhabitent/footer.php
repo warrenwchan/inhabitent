e45f3e3b5072ee16f68983acfb3a15f49eea11ac
Warren Chan <warrenchan@Warrens-MBP.hitronhub.home>
2017-02-23 00:29:58 -0800

    Footer items positioned

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -11,7 +11,26 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+					<div class="info-hour">
+						<div class="contact" >
+							<h2>contact info</h2>
+							<div> <i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:w.warrenchan@gmail.com">w.warrenchan@gmail.com</a> </div>
+							<div> <i class="fa fa-phone" aria-hidden="true"></i><a href="778-456-7891">778-456-7891</a> </div>
+							<div>
+								<a href="facebook.com"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+								<a href="twitter.com"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+								<a href="plus.google.com/"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a>
+							</div>
+						</div>
+						<div class="hours" >
+							<h2>business hours</h2>
+							<p><span>monday-friday:</span> 9am to 5pm</p>
+							<p><span>saturday:</span> 10am to 2pm</p>
+							<p><span>sunday:</span> Closed</p>
+						</div>
+					</div>
+					<img src="<?php echo get_template_directory_uri(); ?>/images/logos/inhabitent-logo-text.svg" alt="Full image of Inhabitent footer logo" />
+					<p class="wp-power" >copyright © 2016 inhabitent</p>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
---
+++ themes/inhabitent/page-products.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: Product Template
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+<p>hello</p>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
