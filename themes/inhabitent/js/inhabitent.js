d39f18e7e7ddeb48fc887706de3710fe3170695f
Warren Chan <warrenchan@Warrens-MBP.hitronhub.home>
2017-02-24 00:59:31 -0800

    front page adventure flexbox

---
+++ themes/inhabitent/js/inhabitent.js
@@ -0,0 +1,11 @@
+// (function( $ ) {
+//     $('#search-toggle').on('click', function() {
+//         $('#search-field').animate( { width: 'toggle' }, 500 ).focus();
+//     });
+
+//     $('#search-field').on('blur', function() {
+//         $('#search-field').animate( { width: 'toggle' }, 500 );
+//     });
+
+//     $('#search-field').animate( { width: 'toggle' }, 0 );
+// })( jQuery ); 
--- themes/inhabitent/page-templates/front-page.php
+++ themes/inhabitent/page-templates/front-page.php
@@ -6,83 +6,71 @@
  */
 
 ?>
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<div class="hero" >
-				<?php get_header('home'); ?>
-				<img src="<?php echo get_template_directory_uri(); ?>/images/logos/inhabitent-logo-full.svg" alt="Full image of Inhabitent logo" />
-			</div>
-
-			<section>
-				<h2>Shop Stuff</h2>
-                <?php  //shop stuff on front page
-                    $arg = array( 'taxonomy' => 'product_type', 'hide_empty' => true);
-                    $terms = get_terms( $arg );
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<div class="hero" >
+			<?php get_header('home'); ?>
+			<img src="<?php echo get_template_directory_uri(); ?>/images/logos/inhabitent-logo-full.svg" alt="Full image of Inhabitent logo" />
+		</div>
+		<section>
+			<h2>Shop Stuff</h2>
+            <?php  //shop stuff on front page
+                $arg = array( 'taxonomy' => 'product_type', 'hide_empty' => true);
+                $terms = get_terms( $arg );
+            ?>
+            <div class="product-list-style">
+				
+                <ul>
+                    <?php foreach ( $terms as $term ) : ?>
+						<?php $url = get_term_link($term->slug, 'product_type'); ?>
+                        <li>
+							<img src="<?php echo get_template_directory_uri(); ?>/images/product-type-icons/<?php echo $term->slug; ?>.svg" alt="Product icon image of <?php echo $term->slug;?> ">
+							<p><?php echo $term->description; ?></p>
+							<a href="<?php echo get_term_link($term); ?>"><?php echo $term->name; ?></a>
+						</li>
+                    <?php endforeach; ?>
+                </ul>
+                <?php
+                    the_archive_description( '<div e="taxonomy-description">', '</div>' );
                 ?>
-
-                <div class="product-list-style">
-					
-                    <ul>
-                        <?php foreach ( $terms as $term ) : ?>
-							<?php $url = get_term_link($term->slug, 'product_type'); ?>
-                            <li>
-								<img src="<?php echo get_template_directory_uri(); ?>/images/product-type-icons/<?php echo $term->slug; ?>.svg" alt="Product icon image of <?php echo $term->slug;?> ">
-								<p><?php echo $term->description; ?></p>
-								<a href="<?php echo get_term_link($term); ?>"><?php echo $term->name; ?></a>
-							</li>
-                        <?php endforeach; ?>
-                    </ul>
-
-                    <?php
-                        the_archive_description( '<div e="taxonomy-description">', '</div>' );
-                    ?>
-			</section>
-
-			<section>
-				<h2>inhabitent journal</h2>
-				<div class="journal-feature">
-					<?php
-					global $post;
-					$args = array( 'posts_per_page' => 3, 'order'=> 'DESC', 'orderby' => 'post_date' );
-					$postslist = get_posts( $args );
-					foreach ( $postslist as $post ) :
-					setup_postdata( $post ); ?> 
-						<div class="journal-feature-item" >
-							<?php the_post_thumbnail( 'medium' ); ?>
-							<div class="journal-feature-text" >
-								<span class="comments"><?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?></span>
-								<span class="title"><?php the_title(); ?>  </span> 
-								<button class='readmore' type="button">read entry</button>
-							</div>
+		</section>
+		<section>
+			<h2>inhabitent journal</h2>
+			<div class="journal-feature">
+				<?php
+				global $post;
+				$args = array( 'posts_per_page' => 3, 'order'=> 'DESC', 'orderby' => 'post_date' );
+				$postslist = get_posts( $args );
+				foreach ( $postslist as $post ) :
+				setup_postdata( $post ); ?> 
+					<div class="journal-feature-item" >
+						<?php the_post_thumbnail( 'medium' ); ?>
+						<div class="journal-feature-text" >
+							<span class="comments"><?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?></span>
+							<span class="title"><?php the_title(); ?>  </span> 
+							<button class='readmore' type="button">read entry</button>
 						</div>
-					<?php endforeach; wp_reset_postdata(); ?>
-				</div>
-			</section>
-
-			<section>
-				<h2>latest adventure</h2>
-				<div class="adventure-container">
-					 <div class="wrap1" >
-						 <!--<img src="<?php echo get_template_directory_uri(); ?>/images/adventure-photos/canoe-girl.jpg" alt="canoe-girl" />-->
-					 </div>
-					 <div class="wrap1">
-						<div class="wrap2">
-							<!--<img src="<?php echo get_template_directory_uri(); ?>/images/adventure-photos/beach-bonfire.jpg" alt="beach-bonfire" />-->
-						</div>
-						<div class="wrap3">
-							<!--<img src="<?php echo get_template_directory_uri(); ?>/images/adventure-photos/mountain-hikers.jpg" alt="mountain-hikers" />-->
-						</div>
-						<div class="wrap4">
-							<!--<img src="<?php echo get_template_directory_uri(); ?>/images/adventure-photos/night-sky.jpg" alt="night-sky" />-->
-						</div>
-					 </div>
-				</div>
-			</sectioin>
-			
-		</main> <!-- #main -->
-
-	</div> <!-- #primary -->
-
-<?php get_footer(); ?>
+					</div>
+				<?php endforeach; wp_reset_postdata(); ?>
+			</div>
+		</section>
+		<section>
+			<h2>latest adventure</h2>
+			<div class="adventure-container">
+				 <div class="wrap1" ><!--<img src="<?php echo get_template_directory_uri(); ?>/images/adventure-photos/canoe-girl.jpg" alt="canoe-girl" />-->
+				 </div>
+				 <div class="contain-wrap">
+					<div class="wrap2"><!--<img src="<?php echo get_template_directory_uri(); ?>/images/adventure-photos/beach-bonfire.jpg" alt="beach-bonfire" />-->
+					</div>
+					<div class="wrap3"><!--<img src="<?php echo get_template_directory_uri(); ?>/images/adventure-photos/mountain-hikers.jpg" alt="mountain-hikers" />-->
+					</div>
+					<div class="wrap4"><!--<img src="<?php echo get_template_directory_uri(); ?>/images/adventure-photos/night-sky.jpg" alt="night-sky" />-->
+					</div>
+				 </div>
+			</div>
+		</sectioin>
+		<?php get_footer(); ?>
+	</main> <!-- #main -->
+	
+</div> <!-- #primary -->
 
