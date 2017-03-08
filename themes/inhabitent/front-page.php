122e11ed0183b465fa9225d2e7ffa1938f9d6861
Warren Chan <warrenchan@Warrens-MacBook-Pro.local>
2017-03-08 11:21:34 -0800

    Removed header home

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -6,7 +6,7 @@
  */
 
 ?>
-<?php get_header('home'); ?>
+<?php get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
--- themes/inhabitent/page-templates/about.php
+++ themes/inhabitent/page-templates/about.php
@@ -5,7 +5,7 @@
 * @package Inhabitent Theme
 */
 
-get_header('home'); ?>
+get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
