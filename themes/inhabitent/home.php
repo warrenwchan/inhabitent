8591459d2661b9bbbab9d448c98f3403ce3dc783
Warren Chan <warrenchan@Warrens-MacBook-Pro.local>
2017-03-16 11:29:08 -0700

    journal page code cleaned up

--- themes/inhabitent/home.php
+++ themes/inhabitent/home.php
@@ -9,29 +9,21 @@ get_header(); ?>
 	<div class="journal">
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
-			
+
 				<?php if ( have_posts() ) : ?>
 				<div class="post-container">
-					
-					<?php /* Start the Loop */ ?>
-					<?php while ( have_posts() ) : the_post(); ?>
-
+					<?php while ( have_posts() ) : the_post(); ?> <!--start loop-->
 						<?php get_template_part( 'template-parts/content' ); ?>
-
 					<?php endwhile; ?>
 
-						<?php the_posts_navigation(); ?>
+					<?php the_posts_navigation(); ?>
 				
 					<?php else : ?>
-
 						<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
 					<?php endif; ?>
-
 				</div>
-
 				<?php get_sidebar(); ?>
-
+				
 			</main><!-- #main -->
 		</div><!-- #primary -->
 	</div>
